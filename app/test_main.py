22f3e788d79bb5f06dbe5a4ca5f60dbf63803212
shafran <shafran.andriy@gmail.com>
2024-12-21 15:42:40 +0200

    Created tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,8 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
